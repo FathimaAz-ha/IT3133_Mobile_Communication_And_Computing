bb95cd3f6acc0fb5e7d6a0b93d39ebc384f0d410
FathimaAz-ha <2021ict48@stu.vau.ac.lk>
2025-08-27 16:41:29 +0530

    Added Day-02

---
+++ Day-02/myapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Welcome to my Application
+        </p>
+
+        <h4>Created By : Fathima Azha</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day-02/myapp/src/Name.js
@@ -0,0 +1,10 @@
+function Name(){
+    let title = "IT3133 Practicals"; //declaring a variable
+    return(
+        <center><h2><u><i>{title}</i></u></h2></center> //JSX
+
+        //calling a variable - { variable }
+    )
+}
+
+export default Name //importing
