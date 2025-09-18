3e2bae43ef61528f3e2819dee5be6ddd605fb6d6
Fathima Azha <2021ict48@stu.vau.ac.lk>
2025-09-18 11:03:47 +0530

    Update StudentDetails.js

--- Day-04/charts/src/components/StudentDetails.js
+++ Day-04/charts/src/components/StudentDetails.js
@@ -1,27 +1,51 @@
-import Student from "./Student";
+import react, { useState } from 'react'; //importing use state
+import Student from './Student'
+import '../css/style.css'
+function StudentDetails(params) {
+    const [greet, setGreet] = useState("Hello React!"); //initial state using useState
+    let [count, setCount] = useState(1); //setting initial count to 1
+    let [color, setColor] = useState("coral");
 
-import '../css/styles.css'
-function StudentDetails(params){
+    const students = params.studata; //getting data from parent
+    return (
+        <center>
+            <h1>{greet}</h1>
+            <button onClick={() => { setGreet("Hello to React world!") }}> Change Heading </button>
 
-    const students = params.studata; // getting data from the parent - app.js
-    return(
-      
-        <table border={1}>
-            <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Course</th>
-            </tr>
-
-            {
-                //JS code to access the array
-                students.map(s=>
-                    <Student id={s.id} name={s.name} course={s.course}/>
-
+            <h1>{count}</h1>
+            <button onClick={() => {
+                setCount(
+                    count + 1
                 )
-            }
-        </table>
-        
+            }}> Increase </button>
+
+            <button onClick={() => {
+                setCount(
+                    count - 1
+                )
+            }}> Decrease </button>
+
+            <h1 style={{color : color}}>{color}</h1>
+            <button onClick={() => {
+                setGreet("Hey!");
+                    setColor(
+                   "Red"
+                )
+            }}> Change Color </button>
+            <table border={1} cellPadding={10} >
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>COURSE</th>
+                </tr>
+                {
+                    students.map(s =>
+                        <Student id={s.id} name={s.name} course={s.course} />
+                    )
+                }
+
+            </table>
+        </center>
     )
 }
 
